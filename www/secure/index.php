fe64672042bb30dbdc441f7934fb49a41c951397
Dakota Rennemann <rennemannd@gmail.com>
2020-03-23 12:06:01 -0400

    Added basic page routing for secure website

--- www/secure/index.php
+++ www/secure/index.php
@@ -1,31 +1,21 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <style>
-            input, label {
-                display: block;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Shitty Bank (TM)</h1>
-        <b>Sign up</b>
-        <form method="post" action="authenticate.php">
-            <label for="email"> Email: </label>
-            <input id="email" name="email" type="email"/>
-            <label for="password"> Password: </label>
-            <input id="password" name="password" type="password"/>
-            <label for="passwordConfirm"> Confirm password: </label>
-            <input id="passwordConfirm" name="passwordConfirm" type="password"/>
-            <input type="submit" value="Sign up"/>
-        </form>
-        <form method="post" action="transfer.php">
-            <label for="recipient"> Recipient's email: </label>
-            <input id="recipient" name="recipient" type="email"/>
-            <label for="amount"> Amount (in dollars): </label>
-            <input id="amount" name="amount" type="text"/>
+<?php
+$redirect = $_SERVER['REQUEST_URI'];
 
-            <input type="submit" value="Transfer funds"></input>
-        </form>
-    </body>
-</html>
+switch ($redirect) {
+    case '/'  :
+    case ''   :
+        require __DIR__ . '/pages/home.php';
+        break;
+    case '/signin' :
+        require __DIR__ . '/pages/signIn.php';
+        break;
+    case '/signup' :
+        require __DIR__ . '/pages/signUp.php';
+        break;
+    case '/dashboard' :
+        require __DIR__ . '/pages/dashboard.php';
+        break;
+    default:
+        require __DIR__ . '/pages/404.php';
+        break;
+}
