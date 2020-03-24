1a887fb178d2bebc625d44e0fc4e77cd8b4de2d9
Cameron Whtie <cameron.white@ufl.edu>
2020-03-23 20:10:12 -0400

    commit

--- www/secure/pages/signIn.php
+++ www/secure/pages/signIn.php
@@ -27,7 +27,7 @@
                 }
             }).catch(
                 (err) => {
-                    console.log(err);
+                    alert("error");
                 }
             );
         }
